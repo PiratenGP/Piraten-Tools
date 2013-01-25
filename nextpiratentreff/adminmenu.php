e41b4325d4c773e9f7e57fc0e90152d41d1f0146
Martin Stoppler <stoppe@stoppler.de>
2013-01-25 20:11:09 +0100

    Anleitung bei Next-Piratentreff angepasst

--- nextpiratentreff/adminmenu.php
+++ nextpiratentreff/adminmenu.php
@@ -74,14 +74,20 @@
 		</form>
 		
 		<h3>Anleitung</h3>
-		<p>Eine iCal-Datei wird nach einem Suchbegriff durchsucht und der nächste Termin wird herausgesucht. Zum Einbinden den Shortcode [pt-npt] verwenden mit folgenden Variablen:<br />
-		<strong>id=X</strong>: Die ID des Eintrags, s.o.<br />
-		<strong>output="X"</strong>: Was auf der Seite angezeigt werden soll. Dabei können folgende Platzhalter verwendet werden:<br />
-		&nbsp;&nbsp;&nbsp;&nbsp;%TITEL% - Der Titel des Eintrags<br />
-		&nbsp;&nbsp;&nbsp;&nbsp;%TITEL2% - Der Titel des Eintrags, der Teil der mit "remove" definiert wurde wird entfernt<br />
-		&nbsp;&nbsp;&nbsp;&nbsp;%ORT% - Der Ort des Eintrags<br />
-		&nbsp;&nbsp;&nbsp;&nbsp;{} - In geschweiften Klammern können PHP-Datumsangaben definiert werden, für <?php echo date("d.m.Y"); ?> also zum Beispiel {d.m.Y}<br />
-		<strong>remove="X"</strong>: Wird aus dem Titel herausgelöscht, s.o.<br />
-		<strong>else="X"</strong>: Wird angezeigt, wenn kein Termin gefunden wurde<br />
-		<strong>skip=X</strong>: So viele Termine werden bei der Suche übersprungen<br /></p>
+		<p><ol><li>Einen neuen Eintrag erstellen mit der URL zu der ics-Datei, dem gewünschten Suchbegriff (z.B. "Piratentreff), und dem gemünschten Offset*</li><li>Shortcode [pt-npt] in die gewünschten Seiten einbauen.</li></ol>Folgende Parameter können verwendet werden: (X ist Platzhalter)
+		<table class="pttable">
+		<tr><td>id=X</td><td>Die ID des Eintrags, s.o.</td></tr>
+		<tr><td>output="X"</td><td>Was ausgegeben werden soll.</td></tr>
+		<tr><td>remove="X"</td><td>Wird aus dem Titel herausgelöscht, s.u.</td></tr>
+		<tr><td>else="X"</td><td>Wird ausgegeben, wenn kein Termin gefunden wurde</td></tr>
+		<tr><td>skip=X</td><td>So viele Termine werden bei der Suche übersprungen</td></tr>
+		</table>
+		<br />Beim Parameter "output" können folgende Variablen verwendet werden:
+		<table class="pttable">
+		<tr><td>%TITEL%</td><td>Der Titel des Eintrags</td></tr>
+		<tr><td>%TITEL2%</td><td>Der Titel des Eintrags, der Teil der mit "remove" definiert wurde wird entfernt</td></tr>
+		<tr><td>%ORT%</td><td>Der Ort des Eintrags</td></tr>
+		<tr><td>{X}</td><td>In geschweiften Klammern können PHP-Datumsangaben definiert werden, für <em><?php echo date("d.m.Y"); ?></em> also zum Beispiel <em>{d.m.Y}</em></td></tr>
+		</table>
+		</p>
 	</div>
